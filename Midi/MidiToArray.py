33395ea283946fbe9d834dfc5a4ea3cdf80e56d1
Wuelle <simon.wuelker@arcor.de>
2020-04-10 22:21:52 +0200

    Updated the .midi file encoder and decoder

--- Midi/MidiToArray.py
+++ Midi/MidiToArray.py
@@ -1,25 +1,43 @@
+"""
+Zuerst werden alle Eventzeiten auf die samplerate gerundet.(Tempochanges werden ignoriert)
+"""
 import mido
 import numpy as np
 
-file = mido.MidiFile("output.mid") 
+#10 Samples/Second
+samplerate = 100
 
-tps = 4		#4 Ticks pro Sekunde
+file = mido.MidiFile("Files/example.mid") 
+print("Tempo: {}, ticks per beat: {}".format(file.__dict__, file.ticks_per_beat))
+
 state = np.zeros([128])
-array = np.zeros([int(file.length*tps), 128])
+array = np.zeros([int(file.length*samplerate), 128])
 index = 0
 
+def roundEventTime(time):
+	return round(time*samplerate)/samplerate
 
+count = 0
 
 #msg Object structure: {'type': 'note_on', 'time': 0, 'channel': 0, 'note': 1, 'velocity': 100}
-for msg in file.play():
-	print(msg)
-	#calc how many ticks have passed since the last message
-	if msg.time == 0:
-		array[index][msg.note] = 1
-	else:
-		for step in range(round(msg.time * tps)):
-			array[index] = state
-			index += 1
-	state[msg.note] = not state[msg.note]
-array[-1] = state
-np.save("output_array.npy", array)
+#'time' attribute is given in seconds
+for msg in file:
+	#ignore all non-note changes
+	if msg.type == "note_on" or msg.type == "note_off":
+		#Round the msg time to the nearest sample
+		msg.time = roundEventTime(msg.time)
+
+		#if the event is at a new timestep, flush the old state n times to the array
+		if msg.time != 0:
+			for _ in range(int(msg.time*samplerate)):
+				array[count] = state
+				count += 1
+
+
+		#Note events with velocity 0 are considered to be note off events
+		if msg.type == "note_off" or msg.velocity == 0:
+			state[msg.note] = 0
+		else:
+			state[msg.note] = 1
+file.close()
+np.save("Files/output_array", array)
