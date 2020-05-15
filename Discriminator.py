d5c1b1340344ba218bcf9239a6c0d3f4ac540c06
Wuelle <58120269+Wuelle@users.noreply.github.com>
2020-05-15 18:33:23 +0200

    Update Discriminator.py

--- Discriminator.py
+++ Discriminator.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 class discriminator(nn.Module):
-	def __init__(self, in_size = 80, hidden_size = 50, out_size = 1, n_layers = 1, lr = 0.02):
+	def __init__(self, in_size = 80, hidden_size = 50, out_size = 1, n_layers = 1, lr = 0.01, batch_size = 32):
 		super(discriminator, self).__init__()
 
 		self.in_size = in_size
@@ -11,19 +11,17 @@ class discriminator(nn.Module):
 		self.out_size = out_size 
 		self.n_layers = n_layers
 		self.lr = lr
-		self.hidden = (torch.rand(self.n_layers, 1, self.hidden_size), torch.rand(self.n_layers, 1, self.hidden_size))
+		self.batch_size = batch_size
+		self.hidden = (torch.rand(n_layers, batch_size, hidden_size), torch.rand(n_layers, batch_size, hidden_size))
 
 		#Input ins LSTM hat dim3, dim0 ist die sequenz, dim1 ist der batch, dim2 sind die tatsächlichen daten
 		self.lstm = nn.LSTM(in_size, hidden_size, n_layers)
 		self.lin1 = nn.Linear(hidden_size, out_size)
 		self.sigmoid = nn.Sigmoid()
 
-		#BCCLoss weil der Discriminator eine binary classification macht
-		self.criterion = nn.BCELoss()
 		self.optimizer = optim.Adagrad(self.parameters(), lr)
 
-		self.losses_real = []
-		self.losses_fake = []
+		self.losses = []
 		
 
 	def forward(self, input):
@@ -36,4 +34,4 @@ class discriminator(nn.Module):
 		return squeezed
 
 	def reset_hidden(self):
-		self.hidden = (torch.rand(self.n_layers, 1, self.hidden_size), torch.rand(self.n_layers, 1, self.hidden_size))
+		self.hidden = (torch.rand(self.n_layers, self.batch_size, self.hidden_size), torch.rand(self.n_layers, self.batch_size, self.hidden_size))
