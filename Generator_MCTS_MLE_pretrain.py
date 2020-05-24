6beea01cdf9c00de7e53c500bea71064f3ecc309
Wuelle <simon.wuelker@arcor.de>
2020-05-24 20:27:50 +0200

    Updated rollout policy and added midiconverter

--- Generator_MCTS_MLE_pretrain.py
+++ Generator_MCTS_MLE_pretrain.py
@@ -1,5 +1,5 @@
 import mcts
-import Converter
+import MidiTools
 import os
 import random
 import numpy as np
@@ -7,38 +7,41 @@ import numpy as np
 modelsave_path = "C:/Users/Wuelle/Documents/KI-Bundeswettbewerb-2020/BW-KI-2020/models/"
 dataset_path = "C:/Users/Wuelle/Documents/KI-Bundeswettbewerb-2020/Datensatz/notewise/"
 sample_size = 32
-dataloader = Converter.fetch_sample(sample_size, dataset_path, encode=False)
-load_search_tree = False
-mcts.Node.branching_factor = Converter.vocab_size
+dataloader = MidiTools.fetch_sample(sample_size, dataset_path, encode=False)
+load_search_tree = True
+mcts.Node.branching_factor = MidiTools.vocab_size
+
 if load_search_tree:
 	mcts.loadTree(modelsave_path + "Generator_MCTS.pt")
 	start_node = mcts.Node.allNodes[0]
 else:
 	start_node = mcts.Node(None, np.empty(sample_size)-1)
 
-current_node = start_node
-
-sample = next(dataloader)
 
 
-for note in sample:
-	current_node.simulations[note] += 1
-	current_node.scores[note] += 1
+for i in range(1000):
+	print(f"File Nr.{i}")
+
+	current_node = start_node
+	sample = next(dataloader)
+
+	for note in sample:
+		current_node.simulations[note] += 1
+		current_node.scores[note] += 1
 
 
-	if current_node.simulations[note] == 0:
-		previous_node = current_node
-		current_node = mcts.Node(previous_node.id, mcts.expand(current_node.state.copy(), note))
-		previous_node.children[move] = mcts.Node.counter-1
-	else:
-		current_node = mcts.Node.allNodes[current_node.children[note]]
+		if current_node.simulations[note] == 0:
+			previous_node = current_node
+			current_node = mcts.Node(previous_node.id, mcts.expand(current_node.state.copy(), note))
+			previous_node.children[move] = mcts.Node.counter-1
+		else:
+			current_node = mcts.Node.allNodes[current_node.children[note]]
 
-
+print(current_node.simulations)
 
 mcts.saveTree(modelsave_path + "Generator_MCTS.pt")
-
 #TEST
 import Generator_MCTS
 
-generator = Generator_MCTS.generator(sample_size, Converter.vocab_size)
-generator.save_example()
+generator = Generator_MCTS.generator(sample_size, MidiTools.vocab_size)
+generator.save_example(print_output = True)
--- Midi/readMidiEvents.py
+++
@@ -1,8 +0,0 @@
-import mido
-import sys
-
-file = mido.MidiFile(sys.argv[1]) 
-
-#msg Object structure: {'type': 'note_on', 'time': 0, 'channel': 0, 'note': 1, 'velocity': 100}
-for msg in file.play():
-	print(msg)
