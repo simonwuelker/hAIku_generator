5c14225d96aeb5452447fc26e6bad4768c09cd3a
Wuelle <simon.wuelker@arcor.de>
2020-06-20 22:37:03 +0200

    added failsafe for saving models

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#toller blogpost der erklärt wie der rl agent im generator funktioniert:http://karpathy.github.io/2016/05/31/rl/
-#https://medium.com/@jonathan_hui/gan-why-it-is-so-hard-to-train-generative-advisory-networks-819a86b3750b
 #hAIku poem generator
 import torch
 import torch.nn as nn
@@ -11,7 +9,6 @@ import Discriminator
 import Tools
 
 import matplotlib.pyplot as plt
-import matplotlib.collections as collections
 import time
 
 def example():
@@ -22,18 +19,18 @@ def example():
 	single = torch.rand(batch_size, Tools.word2vec_model.vector_size)
 	for i in range(haiku_length):
 		seed[i] = single 
-	loss = 0
+	total_loss = 0
 	output = generator(seed)
 
 	optimal_haiku = torch.empty(haiku_length, 1, Tools.word2vec_model.vector_size)
 
 	for index in range(haiku_length):
 		best = Tools.bestAction(output[:index], haiku_length, discriminator.forward)
-		loss += generator.criterion(output[index, 0], best[0])
+		total_loss += generator.criterion(output[index, 0], best[0])
 
 	# #optimize generator
 	generator.optimizer.zero_grad()
-	loss.backward(retain_graph = True)
+	total_loss.backward(retain_graph = True)
 	generator.optimizer.step()
 
 	print(f"{Tools.decode(output)} Reward: {discriminator.forward(output).item()}")
@@ -66,58 +63,55 @@ generator.train()
 training_time = 1000
 start_time = time.time()
 episode = 0
+try:
+	while time.time() < start_time + training_time:
+		#print progress
+		Tools.progressBar(start_time, time.time(), training_time, episode)
+		episode += 1
+		
+		real_sample = next(dataloader)
+		fake_sample = example()
 
-while time.time() < start_time + training_time:
-	#print progress
-	Tools.progressBar(start_time, time.time(), training_time, episode)
-	episode += 1
-	
-	real_sample = next(dataloader)
-	fake_sample = example()
+		#take outputs from discriminator and log them
+		score_real = discriminator(real_sample)
+		score_fake = discriminator(fake_sample)
 
-	#take outputs from discriminator and log them
-	score_real = discriminator(real_sample)
-	score_fake = discriminator(fake_sample)
+		#Save scores for evaluation
+		discriminator.scores_real.append(score_real.item())
+		discriminator.scores_fake.append(score_fake.item())
+		print(score_real, 1)
+		print(score_fake, 1)
 
-	#Save scores for evaluation
-	discriminator.scores_real.append(score_real.item())
-	discriminator.scores_fake.append(score_fake.item())
-	print(score_real, 1)
-	print(score_fake, 1)
+		#calculate losses(Discriminator minimizes, generator maximizes
+		loss_d = torch.mean(-torch.log(1.1-score_fake) - torch.log(score_real))
+		loss_g = torch.mean(torch.log(score_fake))
 
-	#calculate losses(Discriminator minimizes, generator maximizes
-	loss_d = torch.mean(-torch.log(1.1-score_fake) - torch.log(score_real))
-	loss_g = torch.mean(torch.log(score_fake))
+		#save losses
+		generator.losses.append(loss_g.item())
+		discriminator.losses.append(loss_d.item())
 
-	#save losses
-	generator.losses.append(loss_g.item())
-	discriminator.losses.append(loss_d.item())
+		#optimize discriminator
+		discriminator.optimizer.zero_grad()
+		loss_d.backward()
+		discriminator.optimizer.step()
+finally:	
+	#TESTING
+	discriminator.eval()
+	generator.eval()
 
-	#optimize discriminator
-	discriminator.optimizer.zero_grad()
-	loss_d.backward()
-	discriminator.optimizer.step()
-	
-#TESTING
-discriminator.eval()
-generator.eval()
+	#test without operation tracking to save memory
+	with torch.no_grad():
+		pass
 
-# with torch.no_grad():
-# 	print(Tools.predMaxReward(Tools.encode("concentrate concentrate"), 2, discriminator.forward))
+	torch.save(discriminator.state_dict(), f"{modelsave_path}Discriminator.pt")
+	torch.save(generator.state_dict(), f"{modelsave_path}Generator.pt")
 
-torch.save(discriminator.state_dict(), f"{modelsave_path}Discriminator.pt")
-torch.save(generator.state_dict(), f"{modelsave_path}Generator.pt")
+	#plot the graph of the different losses over time
+	fig, ax = plt.subplots()
+	ax.plot(discriminator.scores_real, label = "Real")
+	ax.plot(discriminator.scores_fake, label = "Fake")
+	plt.ylabel("Scores")
+	plt.xlabel("training duration")
+	ax.legend()
 
-#plot the graph of the different losses over time
-fig, ax = plt.subplots()
-# ax.plot(generator.losses, label = f"Generator")
-# ax.plot(discriminator.losses, label = "Discriminator")
-ax.plot(discriminator.scores_real, label = "Real")
-ax.plot(discriminator.scores_fake, label = "Fake")
-plt.ylabel("Scores")
-plt.xlabel("training duration")
-ax.legend()
-
-plt.show()
-print(discriminator.scores_real)
-print(discriminator.scores_fake)
+	plt.show()
