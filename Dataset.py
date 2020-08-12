f77ddba6fc708bdc93e427a69bd6b1a5c21ae815
Wuelle <simon.wuelker@arcor.de>
2020-08-12 17:22:56 +0200

    removed unnecessary variables

--- Dataset.py
+++ Dataset.py
@@ -1,29 +1,21 @@
 import torch
-from ordered_set import OrderedSet  # OrderedSet ensures that the word-token mappings will always be the same
 import gensim
 
-
 class Dataset(torch.utils.data.Dataset):
-	def __init__(self, path):
-		self.data, self.unique_tokens = self.loadData(path)
+	def __init__(self, path_data, path_model="models/word2vec.model"):
+		# load all the haikus from a file
+		with open(path_data, "r", encoding="utf8", errors="ignore") as infile:
+			self.data = infile.read().splitlines()
 
-		self.word_to_ix = {word: ix for ix, word in enumerate(self.unique_tokens)}
-		self.ix_to_word = {ix: word for ix, word in enumerate(self.unique_tokens)}
 		self.model = gensim.models.KeyedVectors.load("models/word2vec.model")
-		self.embedding_dim = 100  # this is the default value for gensim models
+		self.embedding_dim = self.model.vector_size  # default is 100
 
 	def __len__(self):
 		return len(self.data)
 
 	def __getitem__(self, index):
-		"""Returns a sample of shape [N, Seq_length, 1] at a certain index"""
+		"""Returns a sample of shape [Seq_length, embedding dim] at a certain index"""
 		return self.encode(self.data[index])
-
-	def loadData(self, path):
-		with open(path, "r", encoding="utf8", errors="ignore") as infile:
-			haikus = infile.read().splitlines()
-
-		return haikus, OrderedSet([word for haiku in haikus for word in haiku.split()])
 
 	def encode(self, haiku):
 		""" Encodes a single line of text into either indices or word tensors"""
@@ -35,7 +27,7 @@ class Dataset(torch.utils.data.Dataset):
 		return result
 
 	def decode(self, tensor):
-		""" Decodes either a """
+		# this function is pretty slow
 		batch_size = tensor.shape[0]
 		seq_length = tensor.shape[1]
 
