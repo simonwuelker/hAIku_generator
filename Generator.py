9ae8b1183fe895081482b551eaa774464ac2b717
Wuelle <simon.wuelker@arcor.de>
2020-08-25 20:09:08 +0200

    added Action Noise from Philtabor

--- Generator.py
+++ Generator.py
@@ -6,6 +6,20 @@ import numpy as np
 import random
 
 
+class OUActionNoise:
+	def __init__(self, mu, sigma=0.15, theta=0.2, dt=0.01, x0=None):
+		self.theta = theta
+		self.mu = mu
+		self.sigma = sigma
+		self.dt = dt
+		self.x0 = x0
+		self.x_prev = np.zeros_like(self.mu)
+
+	def __call__(self):
+		x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+		self.x_prev = x
+		return x
+
 class Critic(nn.Module):
 	def __init__(self, beta, n_actions, name, save_path="models/"):
 		super(Critic, self).__init__()
