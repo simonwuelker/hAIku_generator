c691c4e7365e23503e10d09a70e609f08164ed3d
Wuelle <simon.wuelker@arcor.de>
2020-09-25 16:51:21 +0200

    moved main training back in main.py

--- main.py
+++ main.py
@@ -20,16 +20,20 @@ parser = argparse.ArgumentParser(description="Train a SeqGAN model in continuous
 parser.add_argument("--pretrain_w2v", help="Whether to pretrain the Word2Vec Models", action="store_true")
 parser.add_argument("--pretrain_gen", help="Whether to pretrain the generator. Values are [batch size, epochs]", default=None, nargs=2)
 parser.add_argument("--pretrain_dis", help="Whether to pretrain the discriminator. Values are [batch size, epochs]", default=None, nargs=2, type=int)
-parser.add_argument("--train", help="Whether to train the models in GAN fashion. Values are [batch size, epochs]", default=None, nargs=2)
+parser.add_argument("--no_train", help="Whether to train the models in GAN fashion.", action="store_true")
+
 # Paths
 parser.add_argument("--data", help="Specify path to the Dataset", default="data/dataset_clean.txt", dest="data_path")
 parser.add_argument("--models", help="Specify path to the model Directory", default="models", dest="model_path")
 parser.add_argument("--img", help="Specify path to store the training images in", default="training_img", dest="img_path")
 parser.add_argument("--use_pretrained", help="Whether to use the pretrained Models", action="store_true", dest="use_pretrained")
 parser.add_argument("--use_trained", help="Whether to use the trained Models", action="store_true", dest="use_trained")
+
 # Parameters(add more here)
 parser.add_argument("--seed", help="Seed for torch/numpy", default=1, dest="seed", type=int)
 parser.add_argument("--embedding_dim", help="Number of dimensions for word embeddings", type=int, default=80)
+parser.add_argument("--epochs", help="Number of epochs during main training", type=int, default=1)
+parser.add_argument("--batch_size", help="Batch size during main training", type=int, default=5)
 
 # parse the provided arguments
 args = parser.parse_args()
@@ -66,9 +70,73 @@ if args.pretrain_gen is not None:
 if args.pretrain_dis is not None:
 	discriminator = discriminator_pretrain.train(discriminator, dataset, args)
 
-if args.train is not None:
-	generator, discriminator = gan_training.train()
+if not args.no_train:
+	# TRAINING
+	generator.train()
+	discriminator.train()
+	training_progress = tqdm(total = dataset.train_cap * args.epochs, desc = "Training")
 
-# save the models
-generator.saveModel()
-discriminator.saveModel()
+	try:
+		for epoch in range(args.epochs):
+			for real_sample in training_iterator:
+				print(dataset.decode(real_sample))
+				assert False
+				fake_sample = generator.generate(args.batch_size)
+
+				# update the progress bar
+				training_progress.update(args.batch_size)
+
+				# take outputs from discriminator
+				score_real = discriminator(real_sample)
+				score_fake = discriminator(fake_sample)
+
+				# Save scores for evaluation
+				discriminator.scores_real.append(score_real.item())
+				discriminator.scores_fake.append(score_fake.item())
+
+				# calculate loss
+				loss_d = torch.mean(-torch.log(1 - score_fake) - torch.log(score_real))
+				discriminator.losses.append(loss_d.item())
+
+				# optimize discriminator
+				discriminator.learn(loss_d)
+				generator.learn(fake_sample, discriminator)
+
+	finally:
+		# save the models
+		generator.saveModel()
+		discriminator.saveModel()
+
+		# # TESTING
+		# discriminator.eval()
+		# generator.eval()
+
+		# with torch.no_grad():
+		# 	haikus = dataset.decode(generator.generate(10))
+		# 	for haiku in haikus:
+		# 		print(haiku)
+
+		# # smooth out the loss functions (avg of last 25 episodes)
+		# generator.losses = [np.mean(generator.losses[max(0, t-25):(t+1)]) for t in range(len(generator.losses))]
+		# discriminator.losses = [np.mean(discriminator.losses[max(0, t-25):(t+1)]) for t in range(len(discriminator.losses))]
+
+		# plot the graph of the different losses over time
+		fig, (d_score_plot, g_loss_plot, d_loss_plot) = plt.subplots(2, 2, num = "Training Data")
+
+		# Discriminator scores
+		d_score_plot.title.set_text("Discriminator Scores")
+		d_score_plot.plot(discriminator.scores_real, label = "Real")
+		d_score_plot.plot(discriminator.scores_fake, label = "Fake")
+		d_score_plot.legend()
+
+		# Generator Loss
+		g_loss_plot.title.set_text("Generator Loss")
+		g_loss_plot.plot(generator.losses, label = "Generator Loss")
+
+		# Discriminator Loss
+		d_loss_plot.title.set_text("Discriminator Loss")
+		d_loss_plot.plot(discriminator.losses, label = "Discriminator Loss")
+
+		fig.tight_layout()
+		plt.savefig("training_graphs/main.png")
+		plt.show()
